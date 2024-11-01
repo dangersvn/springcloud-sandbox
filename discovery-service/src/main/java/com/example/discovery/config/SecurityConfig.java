6b3322d7d706462eee68fe14d3f10d099afc9d00
Dang Nguyen <dang.nguyen@pureintegration.com>
2024-11-01 09:51:10 -0400

    Upgrade to spring boot 3

--- discovery-service/src/main/java/com/example/discovery/config/SecurityConfig.java
+++ discovery-service/src/main/java/com/example/discovery/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.example.discovery.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfig{
+     @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authz -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> {});
+        
+        return http.build();
     }
 }
