e4d957402fede166d2f8d3bdb822c46258c9ef90
Dang Nguyen <dang.nguyen@pureintegration.com>
2024-11-03 10:35:46 -0500

    Added courses management service

---
+++ courses-management-service/src/main/java/com/example/courses_management_service/CoursesManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.courses_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManagementServiceApplication.class, args);
+	}
+
+}
---
+++ courses-management-service/src/test/java/com/example/courses_management_service/CoursesManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courses_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
